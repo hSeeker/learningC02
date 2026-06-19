6f17b656829470788a14d0151e5165976033eec7
hSeeker <hseeker400@pm.me>
2026-06-19 15:38:13 +0200

    cmd arguments

--- src/main.c
+++ src/main.c
@@ -3,21 +3,33 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-void print_usage() { printf("wrong arguments"); }
+void print_usage() {
+  printf("usage: \n \
+                  -n            -create new database file \n \
+                  -f filename   -specifies path to database file \n \
+                  -r            -reads database istead of writing (writing is the default)");
+}
 
 int main(int argc, char *argv[]) {
-  printf("main\n");
-  test();
-  int c = 0;
   bool newfile = false;
   char *filename = NULL;
-  while ((c = getopt(argc, argv, "nf:")) != -1) {
+  bool read = false;
+
+  int c = 0;
+  while ((c = getopt(argc, argv, "nf:r")) != -1) {
     switch (c) {
     case 'n':
       newfile = true;
       break;
     case 'f':
       filename = optarg;
+      break;
+    case 'r':
+      if (newfile) {
+        printf("Cannot newfile and read at the same time!");
+        return -1;
+      }
+      read = true;
       break;
     default:
       print_usage();
